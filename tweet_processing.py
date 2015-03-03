9a9640d182568ece42bc57837899f1420d3acadb
warshmellow <ddwchensf@gmail.com>
2015-03-03 14:48:47 -0800

    Add two functions to take tweet dump in the the format of what's returned from Twitter Search API and append classification labels

--- tweet_processing.py
+++ tweet_processing.py
@@ -43,6 +43,38 @@ def get_statuses_in_order_from_twitter_api_dump_json_file(filename):
     return tweets_dump['statuses']
 
 
+def apply_and_append_to_twitter_api_dump_json(tweets_dump, key_name, funcs):
+    '''
+    Applies f in funcs to tweet_dump (following iterator order) 
+    then returns a copy of tweet_dump, storing the result as the value
+    associated to key_name.
+    
+    e.g. with tweet_dump, 'labels', [predict], returns copy of tweet_dump with 
+    tweet_dump['labels'] = predict(tweet_dump)
+    '''
+    result = tweets_dump
+    for f in funcs:
+        result = f(result)
+    modified_copy = tweets_dump.copy()
+    modified_copy[key_name] = result
+    return modified_copy
+
+
+def predict_and_append_to_twitter_api_dump_json(tweets_dump, label_name, 
+    predict_func):
+    '''
+    Takes a tweets dump and returns a copy with the result of calling
+    predict_func (a prediction function like a classifier) stored with the
+    key label_name
+    '''
+    return apply_and_append_to_twitter_api_dump_json(
+        tweets_dump, 
+        label_name, [
+            tweet_processing.get_text_in_order_from_twitter_api_dump_json,
+            predict_func,
+            lambda y: map(lambda x: x[1], y)])
+
+
 def main():
     pass
 
