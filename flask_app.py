737e147a7910dc1a25cec60e3e65c55b73da2362
warshmellow <ddwchensf@gmail.com>
2015-03-05 17:51:03 -0800

    Add form to upload json of unlabeled tweets

--- flask_app.py
+++ flask_app.py
@@ -5,18 +5,36 @@ import requests
 import socket
 import time
 import tweet_processing as tp
+from werkzeug import secure_filename
 import ipdb
 
 
 app = Flask(__name__)
 PORT = 5353
+ALLOWED_EXTENSIONS = set(['json'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET'])
+def show_upload_page():
+    return render_template('upload.html')
 
 
 @app.route('/unlabeled_tweets', methods=['POST'])
 def label_tweets():
     "Takes Tweet dump from POST data and returns a Tweet dump with labels"
     # Get Tweet Dump out of request
-    tweets_dump = request.json
+    uploaded_file_name_value = 'file'
+    if request.headers['Content-Type'] == 'application/json':
+        tweets_dump = request.json
+
+    elif uploaded_file_name_value in request.files:
+        uploaded_file = request.files[uploaded_file_name_value]
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            tweets_dump = json.loads(uploaded_file.stream.read())
 
     # Predict using classification model
     labeled_tweets_dump = tp.predict_and_append_to_twitter_api_dump_json(
