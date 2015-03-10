ec9b32f8656fbe1828e2c6627ed7a332d6963d95
warshmellow <ddwchensf@gmail.com>
2015-03-10 13:58:10 -0700

    Change classification model to have new training set, count vectorization, naive bayes

--- build_model.py
+++ build_model.py
@@ -1,9 +1,9 @@
 '''
 Trains and pickles the classifier (model) used for the app.
 
-The one below is a Bernoulli Naive Bayes model that takes a the file
-train_02032015.csv, and does NO text preprocessing, only direct
-binary count vectorization.
+The one below is a Multinomial Naive Bayes model that takes a the file
+train_02032015.csv, and does English stopword removal, only direct 
+count vectorization.
 
 The classification model expects the tweets to be classified as a list of
 strings.
@@ -12,7 +12,7 @@ strings.
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn.metrics import classification_report
 from sklearn.cross_validation import KFold
 import cPickle as pickle
@@ -20,8 +20,8 @@ import cPickle as pickle
 
 class ClassificationModel():
     def __init__(self):
-        self.vectorizer = CountVectorizer(binary=True)
-        self.model = BernoulliNB()
+        self.vectorizer = CountVectorizer(stop_words='english')
+        self.model = MultinomialNB()
 
     def fit(self, train_data, train_labels):
         '''
@@ -62,7 +62,7 @@ def load_model(filename):
 
 
 if __name__ == '__main__':
-    X, y = get_data('data/train_02032015.csv')
+    X, y = get_data('data/train_03102015.csv')
     model = ClassificationModel()
     model.fit(X, y)
     with open('model.pkl', 'w') as f:
